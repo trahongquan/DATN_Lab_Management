582e827c2afcd2fe1f031db25446efe5981a2f0a
trahongquan <lovenevertolate97@gmail.com>
2024-05-16 20:29:11 +0700

    Done Dark Theme user side

--- Lab_Management.v1/src/main/resources/static/js/app.js
+++ Lab_Management.v1/src/main/resources/static/js/app.js
@@ -199,18 +199,18 @@ var currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
+    if (currentTheme === 'darkAdmin') {
         toggleSwitch.checked = true;
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'darkAdmin');
+        localStorage.setItem('theme', 'darkAdmin');
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'lightAdmin');
+        localStorage.setItem('theme', 'lightAdmin');
     }
 }
 
